31d685437cf090385f387976212ba8b21b9b9092
ncko <hello@ncko.me>
2024-08-26 21:21:35 -0700

    reformat which-key; move leader-h keyamps to leader-g

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -1,19 +1,19 @@
 return {
-	{
-		'folke/which-key.nvim',
-		event = 'VimEnter',
-		config = function()
-			require('which-key').setup()
+  {
+    'folke/which-key.nvim',
+    event = 'VimEnter',
+    config = function()
+      require('which-key').setup()
 
-			require('which-key').add {
-				{ '<leader>c', group = '[C]ode' },
-				{ '<leader>d', group = '[D]ocument' },
-				{ '<leader>r', group = '[R]ename' },
-				{ '<leader>s', group = '[S]earch' },
-				{ '<leader>w', group = '[W]orkspace' },
-				{ '<leader>t', group = '[T]oggle' },
-				{ '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
-			}
-		end
-	}
+      require('which-key').add {
+        { '<leader>c', group = '[C]ode' },
+        { '<leader>d', group = '[D]ocument' },
+        { '<leader>r', group = '[R]ename' },
+        { '<leader>s', group = '[S]earch' },
+        { '<leader>w', group = '[W]orkspace' },
+        { '<leader>t', group = '[T]oggle' },
+        { '<leader>g', group = '[G]it', mode = { 'n', 'v' } },
+      }
+    end,
+  },
 }
